545a747518c3b514084bbaeb7ea2eb30c87d3624
locdeptrai <loctranvan23.1.2003@gmail.com>
2024-08-18 08:52:12 +0700

    ok

--- XuongMay.Core/obj/Debug/net8.0/XuongMay.Core.AssemblyInfo.cs
+++ XuongMay.Core/obj/Debug/net8.0/XuongMay.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XuongMay.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b35be8c76ec8bf04e353b981b0abc16ef40b961")]
 [assembly: System.Reflection.AssemblyProductAttribute("XuongMay.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XuongMay.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
